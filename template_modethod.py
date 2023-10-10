bd19d24fbaaa59256a46c900bba821fe8919f040
Jin-Seoung <jinseoungoh@gmail.com>
2023-10-10 20:43:55 +0900

    Create template_modethod.py

---
+++ template_modethod.py
@@ -0,0 +1,17 @@
+## Example
+## basic_example.py
+from abc import ABC, abstractmethod
+
+calss Template(ABC)
+   def __init__(self):
+       pass
+
+   @abstractmethod
+   def func1(self):
+       pass
+
+   @abstractmethod
+   def func2(self):
+       pass
+
+    
